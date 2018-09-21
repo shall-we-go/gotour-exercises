85cb9d626f5dc463e92d212e322896bbe1216ca1
Choopong Choosamer <cchoopong@gmail.com>
2018-09-21 17:12:32 +0700

    solution for https://tour.golang.org/methods/23

--- methods-23-rot13reader.go
+++ methods-23-rot13reader.go
@@ -13,6 +13,25 @@ type rot13Reader struct {
 	r io.Reader
 }
 
+func (this rot13Reader) Read(b []byte) (int, error) {
+	b = b[0:0]
+	for {
+		temp := make([]byte, 1)
+		_, err := this.r.Read(temp)
+		if err == io.EOF {
+			break
+		}
+		A := byte('A')
+		if (temp[0] >= A && temp[0] <= A+13) || (temp[0] >= A+26 && temp[0] <= A+26+13) {
+			temp[0] += 13
+		} else if (temp[0] > A+13 && temp[0] <= A+26) || (temp[0] > A+26+13 && temp[0] <= A+26+13+26) {
+			temp[0] -= 13
+		}
+		b = append(b, temp[0])
+	}
+	return len(b), io.EOF
+}
+
 func main() {
 	s := strings.NewReader("Lbh penpxrq gur pbqr!")
 	r := rot13Reader{s}
