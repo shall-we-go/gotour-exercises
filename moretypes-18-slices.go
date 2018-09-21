7d609e62ceb4e6d2ea75d0693b5f3f13c493da32
Choopong Choosamer <cchoopong@gmail.com>
2018-09-21 16:47:51 +0700

    solution for https://tour.golang.org/moretypes/18

--- moretypes-18-slices.go
+++ moretypes-18-slices.go
@@ -6,6 +6,15 @@ package main
 import "golang.org/x/tour/pic"
 
 func Pic(dx, dy int) [][]uint8 {
+	pic := make([][]uint8, dx)
+	for x := 0; x < dx; x++ {
+		picRow := make([]uint8, dy)
+		pic[x] = picRow
+		for y := 0; y < dy; y++ {
+			pic[x][y] = uint8(x ^ y)
+		}
+	}
+	return pic
 }
 
 func main() {
