65b1e3743499ee109107eef085eb5e90e0018cb7
Choopong Choosamer <cchoopong@gmail.com>
2018-09-21 17:04:34 +0700

    solution for https://tour.golang.org/methods/20

--- methods-20-errors.go
+++ methods-20-errors.go
@@ -5,10 +5,35 @@ package main
 
 import (
 	"fmt"
+	"math"
 )
 
+type ErrNegativeSqrt float64
+
+func (this ErrNegativeSqrt) Error() string {
+	return fmt.Sprintf("cannot Sqrt negative number: %v", float64(this))
+}
+
 func Sqrt(x float64) (float64, error) {
-	return 0, nil
+	if x < 0 {
+		return 0, ErrNegativeSqrt(x)
+	}
+	if x == 0 || x == 1 {
+		return x, nil
+	}
+	start := 0.0
+	end := x
+	mid := (start + end) / 2
+	precision := 1e-10
+	for math.Abs(mid*mid-x) > precision {
+		if mid*mid > x {
+			end = mid
+		} else {
+			start = mid
+		}
+		mid = (start + end) / 2
+	}
+	return mid, nil
 }
 
 func main() {
