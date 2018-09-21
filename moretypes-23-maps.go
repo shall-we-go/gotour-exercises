7c353e0c6da5fcf947280df9c94396325fdd5a85
Choopong Choosamer <cchoopong@gmail.com>
2018-09-21 16:50:16 +0700

    solution for https://tour.golang.org/moretypes/23

--- moretypes-23-maps.go
+++ moretypes-23-maps.go
@@ -4,11 +4,17 @@ https://tour.golang.org/moretypes/23
 package main
 
 import (
+	"strings"
+
 	"golang.org/x/tour/wc"
 )
 
 func WordCount(s string) map[string]int {
-	return map[string]int{"x": 1}
+	wordCountMap := make(map[string]int)
+	for _, v := range strings.Fields(s) {
+		wordCountMap[v]++
+	}
+	return wordCountMap
 }
 
 func main() {
