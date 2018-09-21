61eeaf0a4b0402c8d49aed2bcba2263fd4af3f90
Choopong Choosamer <cchoopong@gmail.com>
2018-09-21 16:52:55 +0700

    solution for https://tour.golang.org/moretypes/26

--- moretypes-26-fibonacci-closure.go
+++ moretypes-26-fibonacci-closure.go
@@ -7,12 +7,25 @@ import "fmt"
 
 // fibonacci is a function that returns
 // a function that returns an int.
-func fibonacci() func() int {
+func fibonacci() func(int) int {
+	fibonacciFunc := func(num int) int {
+		a := 1
+		b := 0
+		temp := 0
+		for num >= 0 {
+			temp = a
+			a = a + b
+			b = temp
+			num--
+		}
+		return b
+	}
+	return fibonacciFunc
 }
 
 func main() {
 	f := fibonacci()
 	for i := 0; i < 10; i++ {
-		fmt.Println(f())
+		fmt.Println(f(i))
 	}
 }
