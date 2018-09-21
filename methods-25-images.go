5aec6e8ccc5ddb6fe0cf2623f6780a58b99bdc2e
Choopong Choosamer <cchoopong@gmail.com>
2018-09-21 17:21:00 +0700

    solution for https://tour.golang.org/methods/25

--- methods-25-images.go
+++ methods-25-images.go
@@ -3,11 +3,33 @@ https://tour.golang.org/methods/25
 */
 package main
 
-import "golang.org/x/tour/pic"
+import (
+	"image"
+	"image/color"
 
-type Image struct{}
+	"golang.org/x/tour/pic"
+)
+
+type Image struct {
+	x int
+	y int
+	w int
+	h int
+}
+
+func (this Image) Bounds() image.Rectangle {
+	return image.Rect(this.x, this.y, this.w, this.h)
+}
+
+func (this Image) ColorModel() color.Model {
+	return color.RGBAModel
+}
+
+func (this Image) At(x, y int) color.Color {
+	return color.RGBA{0, 255, 0, 255}
+}
 
 func main() {
-	m := Image{}
+	m := Image{0, 0, 200, 100}
 	pic.ShowImage(m)
 }
