007d027edd2f71c6959ddedf7c280287dd136755
Choopong Choosamer <cchoopong@gmail.com>
2018-09-23 09:42:42 +0700

    fix bug and make if condition is easier to understand

--- methods-23-rot13reader.go
+++ methods-23-rot13reader.go
@@ -15,16 +15,15 @@ type rot13Reader struct {
 
 func (this rot13Reader) Read(b []byte) (int, error) {
 	b = b[0:0]
-	fmt.Println(test)
 	for {
 		temp := make([]byte, 1)
 		_, err := this.r.Read(temp)
 		if err == io.EOF {
 			break
 		}
-		if (temp[0] >= 'A' && temp[0] < 'A'+13) || (temp[0] >= 'a' && temp[0] < 'a'+13) {
+		if (temp[0] >= 'A' && temp[0] <= 'L') || (temp[0] >= 'a' && temp[0] <= 'l') {
 			temp[0] += 13
-		} else if (temp[0] >= 'A'+13 && temp[0] <= 'A'+26) || (temp[0] >= 'a'+13 && temp[0] <= 'a'+26) {
+		} else if (temp[0] >= 'M' && temp[0] <= 'Z') || (temp[0] >= 'm' && temp[0] <= 'z') {
 			temp[0] -= 13
 		}
 		b = append(b, temp[0])
